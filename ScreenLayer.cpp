b020e72885300253b8a6d644bbf1b63fab901823
Cdaragorn1 <Cdaragorn1@95caec72-788d-11de-8223-e15ac962e7eb>
2009-08-11 22:40:35 +0000

    Our efforts to speed things up with hardware surfaces failed, reloaded at revision 60
    git-svn-id: http://wiitanks2.googlecode.com/svn/trunk@63 95caec72-788d-11de-8223-e15ac962e7eb

--- ScreenLayer.cpp
+++ ScreenLayer.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
 #include "ScreenLayer.h"
-using namespace std;
 
 ScreenLayer::ScreenLayer() : VideoLayer()
 {
@@ -16,13 +14,7 @@ bool ScreenLayer::setVideoMode()
 {
     //SDL_RESIZABLE
     surface = SDL_SetVideoMode(mWidth, mHeight, 0,
-        SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
+        SDL_SWSURFACE);
     if (surface == NULL) return false;
-
-    cerr << "hw available? " << (SDL_GetVideoInfo()->hw_available ? "yes" : "no") << endl;
-    cerr << "flag check: " << surface->flags << endl;
-    //cerr << "flag check: " << (surface->flags & SDL_HWSURFACE) << endl;
-    cerr << "hardware surface? " << ((surface->flags & SDL_HWSURFACE) > 0 ? "yes" : "no") << endl;
-
     return true;
 }
--- VideoLayer.cpp
+++ VideoLayer.cpp
@@ -21,7 +21,6 @@ VideoLayer::~VideoLayer()
 SDL_Surface* VideoLayer::getImage(const char* inFile)
 {
     SDL_Surface* t = NULL;
-    SDL_Surface* u = NULL;
     SDL_Surface* outSurface = NULL;
 
     if((t = IMG_Load(inFile)) == NULL)
@@ -30,29 +29,8 @@ SDL_Surface* VideoLayer::getImage(const char* inFile)
         return NULL;
     }
 
-    Uint32 rmask, gmask, bmask, amask;
-
-    if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
-    {
-        rmask = 0xff000000;
-        gmask = 0x00ff0000;
-        bmask = 0x0000ff00;
-        amask = 0x000000ff;
-    }
-    else
-    {
-        rmask = 0x000000ff;
-        gmask = 0x0000ff00;
-        bmask = 0x00ff0000;
-        amask = 0xff000000;
-    }
-
-    u = SDL_DisplayFormatAlpha(t);
+    outSurface = SDL_DisplayFormatAlpha(t);
     SDL_FreeSurface(t);
-    outSurface = SDL_CreateRGBSurface(SDL_HWSURFACE, u->w, u->h,
-        32, rmask, gmask, bmask, amask);
-    SDL_BlitSurface(u, NULL, outSurface, NULL);
-    SDL_FreeSurface(u);
 
     return outSurface;
 }
