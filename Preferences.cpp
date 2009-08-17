6492399d1404eb91f385f4e73c80e9c5addaf5b9
Cdaragorn1 <Cdaragorn1@95caec72-788d-11de-8223-e15ac962e7eb>
2009-08-17 21:11:06 +0000

    Added preferences files
    git-svn-id: http://wiitanks2.googlecode.com/svn/trunk@84 95caec72-788d-11de-8223-e15ac962e7eb

---
+++ Preferences.cpp
@@ -0,0 +1,79 @@
+#include "Preferences.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+//setup all our static variables
+int Preferences::mWidth = 800;
+int Preferences::mHeight = 600;
+ofstream Preferences::prefs;
+
+/*
+*   setup()
+*
+*   Loads all the preferences from a presaved file
+*
+*   If the file is not found, it creates one and writes the default settings out to it.
+*/
+void Preferences::setup()
+{
+    ifstream settings;
+    settings.open("settings.dat");
+
+    string next;
+
+    if (settings.fail())
+    {
+        cerr << "Error opening settings\n";
+        ofstream defSet;
+
+        defSet.open("settings.dat");
+
+        if (defSet.fail())
+        {
+            cerr << "Error creating settings file" << endl;
+        }
+        else
+        {
+            defSet << mWidth << endl << mHeight << endl;
+        }
+    }
+    else
+    {
+
+        getline(settings, next);
+
+        mWidth = atoi(next.substr(next.find_first_of("0123456789")).c_str());
+
+        getline(settings, next);
+
+
+        mHeight = atoi(next.substr(next.find_first_of("0123456789")).c_str());
+
+
+        settings.close();
+    }
+}
+
+int Preferences::getScreenWidth()
+{
+    return mWidth;
+}
+
+int Preferences::getScreenHeight()
+{
+    return mHeight;
+}
+
+void Preferences::setScreenWidth(int inWidth)
+{
+    prefs.open("settings.dat");
+
+    if (!prefs.fail())
+    {
+    }
+}
+
+void Preferences::setScreenHeight(in inHeight)
+{
+}
---
+++ Preferences.h
@@ -0,0 +1,26 @@
+#ifndef _PREFERENCES_H
+#define _PREFERENCES_H
+
+#include <fstream>
+using namespace std;
+
+class Preferences
+{
+    public:
+        static void setup();
+
+        static int getScreenWidth();
+        static int getScreenHeight();
+
+        static void setScreenWidth(int inWidth);
+        static void setScreenHeight(int inHeight);
+
+    private:
+        static int mWidth;
+        static int mHeight;
+
+        static ofstream prefs;
+
+};
+
+#endif
