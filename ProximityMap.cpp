dbd1a045479b4e4da8be25e62e5e31aac76556a4
TheBuzzSaw <TheBuzzSaw@95caec72-788d-11de-8223-e15ac962e7eb>
2009-08-19 22:03:45 +0000

    yarr
    git-svn-id: http://wiitanks2.googlecode.com/svn/trunk@106 95caec72-788d-11de-8223-e15ac962e7eb

--- ProximityMap.cpp
+++ ProximityMap.cpp
@@ -1,1 +1,14 @@
 #include "ProximityMap.h"
+
+ProximityMap::ProximityMap(GameMap* inMap, int inProximitySize)
+{
+    mProximitySize = inProximitySize;
+    mGMapWidth = inMap->getWidth();
+    mGMapHeight = inMap->getHeight();
+
+    mPMapWidth = mGMapWidth / (mProximitySize - 1);
+}
+
+ProximityMap::~ProximityMap()
+{
+}
--- ProximityMap.h
+++ ProximityMap.h
@@ -1,8 +1,25 @@
 #ifndef _PROXIMITYMAP_H
 #define	_PROXIMITYMAP_H
 
+#include "GameMap.h"
+
+struct Proximity
+{
+    int numEntities;
+    // pointer to linked list
+};
+
 class ProximityMap
 {
+    public:
+        ProximityMap(GameMap* inMap, int inProximitySize);
+        virtual ~ProximityMap();
+    private:
+        int mProximitySize;
+        int mGMapWidth;
+        int mGMapHeight;
+        int mPMapWidth;
+        int mPMapHeight;
 };
 
 #endif
